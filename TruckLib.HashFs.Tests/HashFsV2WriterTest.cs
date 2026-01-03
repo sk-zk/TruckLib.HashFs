502d8a8f227f1414ffb41eda22138ae06b1b8141
sk-zk <sk-zk@users.noreply.github.com>
2026-01-03 17:54:10 +0100

    Add test

--- TruckLib.HashFs.Tests/HashFsV2WriterTest.cs
+++ TruckLib.HashFs.Tests/HashFsV2WriterTest.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TruckLib.HashFs.HashFsV2;
 
 namespace TruckLib.HashFs.Tests
 {
-    public class HashFsV1WriterTest
+    public class HashFsV2WriterTest
     {
         [Fact]
         public void AddFromFsAndSave()
         {
-            var writer = new HashFsV1Writer() 
+            var writer = new HashFsV2Writer() 
             { 
                 Salt = 42,
             };
@@ -30,9 +31,9 @@ namespace TruckLib.HashFs.Tests
 
             using var reader = HashFsReader.Open(outStream);
 
-            Assert.Equal(1, reader.Version);
+            Assert.Equal(2, reader.Version);
             Assert.Equal(42, reader.Salt);
-            Assert.Equal(15, reader.Entries.Count);
+            Assert.Equal(14, reader.Entries.Count);
 
             Assert.True(reader.DirectoryExists("/"));
             Assert.True(reader.FileExists("/manifest.sii"));
@@ -48,6 +49,15 @@ namespace TruckLib.HashFs.Tests
             Assert.False(fileEntry.IsDirectory);
             Assert.Equal(0x3C6369BC6EFDD668UL, fileEntry.Hash);
 
+            var tobjEntry = (EntryV2)reader.GetEntry("/model/simple_cube/cubetx.tobj");
+            Assert.True(tobjEntry.IsCompressed);
+            Assert.NotNull(tobjEntry.TobjMetadata);
+            Assert.Equal(256u, tobjEntry.TobjMetadata.Value.TextureWidth);
+            Assert.Equal(256u, tobjEntry.TobjMetadata.Value.TextureHeight);
+            Assert.Equal(HashFs.Dds.DxgiFormat.BC1_UNORM_SRGB, tobjEntry.TobjMetadata.Value.Format);
+            Assert.Equal(9u, tobjEntry.TobjMetadata.Value.MipmapCount);
+            Assert.False(tobjEntry.TobjMetadata.Value.IsCube);
+
             var extracted = reader.ReadAllText("/def/world/model.tests.sii");
             var original = File.ReadAllText("Data/SampleMod/def/world/model.tests.sii");
             Assert.Equal(original, extracted);
@@ -56,7 +66,7 @@ namespace TruckLib.HashFs.Tests
         [Fact]
         public void ThrowIfArchivePathIsEmptyString()
         {
-            var writer = new HashFsV1Writer();
+            var writer = new HashFsV2Writer();
             using var ms = new MemoryStream();
             Assert.Throws<ArgumentNullException>(() => writer.Add(ms, ""));
             Assert.Throws<ArgumentNullException>(() => writer.Add(ms, null));
@@ -65,7 +75,7 @@ namespace TruckLib.HashFs.Tests
         [Fact]
         public void ThrowIfArchivePathIsRoot()
         {
-            var writer = new HashFsV1Writer();
+            var writer = new HashFsV2Writer();
             using var ms = new MemoryStream();
             Assert.Throws<ArgumentException>(() => writer.Add(ms, "/"));
         }
